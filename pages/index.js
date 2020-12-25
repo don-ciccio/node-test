e8636256d5f730697a971878a35103e52f544ea7
Danilo D'Alessio <danilo.dalessio@gmail.com>
2020-12-25 13:27:58 +0100

    website barebone

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,47 @@
-function HomePage() {
-    return (
-        <div>
-            <h1>Welcome to my first Next app!</h1>
-        </div>
-    );
-}
+import Link from 'next/link';
+import fs from 'fs';
+import matter from 'gray-matter';
+
+const HomePage = (props) => {
+    return props.products.map((product) => {
+        return (
+            <div>
+                <Link href={product.slug}>
+                    <a>
+                        <h1>{product.name}</h1>
+                    </a>
+                </Link>
+                <div>
+                    <p>{product.description}</p>
+                    <p>${product.price / 100}</p>
+                </div>
+            </div>
+        );
+    });
+};
+
+export const getStaticProps = async () => {
+    const directory = `${process.cwd()}/content`;
+    const filenames = fs.readdirSync(directory);
+
+    const products = filenames.map(filename => {
+        // read file from fs
+        const fileContent = fs.readFileSync(`${directory}/${filename}`).toString();
+        // pull out formatter => name
+        const { data } = matter(fileContent);
+        // return name, slug
+        const slug = `/products/${filename.replace(".md", "")}`;
+        const product = {
+            ...data,
+            slug,
+        };
+        return product;
+    });
+    return {
+        props: {
+            products,
+        },
+    };
+};
 
 export default HomePage;
